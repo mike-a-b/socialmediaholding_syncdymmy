55ea38ede2923c02ee78bea3ed26f66f15d74dcc
Mihail <mike.acidburn@gmail.com>
2025-04-07 11:01:42 +0300

    add sync controller, error dns resolve

---
+++ app/Http/Controllers/ProductSyncController.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Http;
+use App\Models\Product;
+
+class ProductSyncController extends Controller
+{
+    public function syncProducts()
+    {
+        $response = Http::timeout(20)->get('https://dummyjson.com/products/search?q=iphone');
+        if (!$response->successful()) {
+            return response()->json(['error' => 'Failed to fetch from API'], 500);
+        }
+
+        $products = $response->json()['products'];
+
+        foreach ($products as $item) {
+            dd($item);
+            Product::updateOrCreate(
+                ['external_id' => $item['id'], 'type' => 'product'],
+                [
+                    'title' => $item['title'],
+                    'description' => $item['description'],
+                    'price' => $item['price'],
+                    'brand' => $item['brand'],
+                ]
+            );
+        }
+
+        return response()->json(['message' => 'Products synced', 'count' => count($products)]);
+    }
+
+    public function getProducts()
+    {
+        return Product::where('type', 'product')->get();
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use Faker\Factory;
+use App\Http\Controllers\ProductSyncController;
+
+Route::get('/sync/products', [ProductSyncController::class, 'syncProducts']);
+Route::get('/products', [ProductSyncController::class, 'getProducts']);
 
 Route::get('/', function () {
     return view('welcome');
